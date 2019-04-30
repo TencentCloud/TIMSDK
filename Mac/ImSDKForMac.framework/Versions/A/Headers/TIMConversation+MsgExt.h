e7281d933c327d26ad8aa5e9268d101ab345dbde
lynxzhang <lynxzhang1988@gmail.com>
2019-04-30 17:23:21 +0800

    发布 4.3.81 版本

--- Mac/ImSDKForMac.framework/Versions/A/Headers/TIMConversation+MsgExt.h
+++ Mac/ImSDKForMac.framework/Versions/A/Headers/TIMConversation+MsgExt.h
@@ -15,78 +15,95 @@
 @interface TIMConversation (MsgExt)
 
 /**
- *  保存消息到消息列表，这里只保存在本地
+ *  保存会话消息
+ *
+ *  保存消息到消息列表，这里只保存在本地。
  *
  *  @param msg 消息体
  *  @param sender 发送方
  *  @param isReaded 是否已读，如果发送方是自己，默认已读
  *
- *  @return 0 成功
+ *  @return 0：成功；1：失败
  */
 - (int)saveMessage:(TIMMessage*)msg sender:(NSString*)sender isReaded:(BOOL)isReaded;
 
 /**
- *  获取会话消息
+ *  获取会话漫游消息
+ *
+ *  1. 登录后可以获取漫游消息，单聊和群聊消息免费漫游7天，用户有额外消息漫游需求时，可前往 [IM 控制台](https://console.cloud.tencent.com/avc) -> 功能配置 -> 消息 ->消息漫游时长 购买，具体资费请参考 [价格说明](https://cloud.tencent.com/document/product/269/11673)。
+ *  2. 如果本地消息全部都是连续的，则不会通过网络获取，如果本地消息不连续，会通过网络获取断层消息。
+ *  3. 对于图片、语音等资源类消息，消息体只会包含描述信息，需要通过额外的接口下载数据，详情请参考 [消息收发]（https://cloud.tencent.com/document/product/269/9150）中的 "消息解析" 部分文档。
  *
  *  @param count 获取数量
- *  @param last  上次最后一条消息
+ *  @param last  上次最后一条消息，如果 last 为 nil，从最新的消息开始读取
  *  @param succ  成功时回调
  *  @param fail  失败时回调
  *
- *  @return 0 本次操作成功
+ *  @return 0：本次操作成功；1：本次操作失败
  */
 - (int)getMessage:(int)count last:(TIMMessage*)last succ:(TIMGetMsgSucc)succ fail:(TIMFail)fail;
 
 /**
- *  向前获取会话消息
+ *  向前获取会话漫游消息
+ *
+ *  调用方式和 getMessage 一样，区别是 getMessage 获取的是时间更老的消息，主要用于下拉 Tableview 刷新消息数据，getMessageForward 获取的是时间更新的消息，主要用于上拉 Tableview 刷新消息数据。
  *
  *  @param count 获取数量
- *  @param last  上次最后一条消息
+ *  @param last  上次最后一条消息，如果 last 为 nil，从最新的消息开始读取
  *  @param succ  成功时回调
  *  @param fail  失败时回调
  *
- *  @return 0 本次操作成功
+ *  @return 0：本次操作成功；1：本次操作失败
  */
 - (int)getMessageForward:(int)count last:(TIMMessage*)last succ:(TIMGetMsgSucc)succ fail:(TIMFail)fail;
 
 /**
  *  获取本地会话消息
  *
+ *  1. 对于单聊，登录后可以获取本地缓存消息
+ *  2. 对于群组，开启最近联系人漫游（登录之后 SDK 默认开启，可以通过 TIMUserConfig 里面的 disableRecnetContact 关闭）的情况下，登录后只能获取最近一条消息，可通过 getMessage 获取漫游消息
+ *
  *  @param count 获取数量
  *  @param last  上次最后一条消息
  *  @param succ  成功时回调
  *  @param fail  失败时回调
  *
- *  @return 0 本次操作成功
+ *  @return 0：本次操作成功；1：本次操作失败
  */
 - (int)getLocalMessage:(int)count last:(TIMMessage*)last succ:(TIMGetMsgSucc)succ fail:(TIMFail)fail;
 
 /**
- *  获取会话消息
+ *  获取指定会话消息
  *
  *  @param locators 消息定位符（TIMMessageLocator）数组
  *  @param succ  成功时回调
  *  @param fail  失败时回调
  *
- *  @return 0 本次操作成功
+ *  @return 0：本次操作成功；1：本次操作失败
  */
 - (int)findMessages:(NSArray*)locators succ:(TIMGetMsgSucc)succ fail:(TIMFail)fail;
 
 /**
- *  撤回消息（仅C2C和GROUP会话有效、onlineMessage无效、AVChatRoom和BChatRoom无效）
+ *  撤回消息
+ *
+ *  1. 仅 C2C 和 GROUP 会话有效、onlineMessage 无效、AVChatRoom 和 BChatRoom 无效。
+ *  2. 可以在 TIMUserConfig 设置的 TIMMessageRevokeListener 监听 onRevokeMessage 消息撤回回调。
+ *  3. 遍历本地消息列表，如果消息的 respondsToLocator 函数返回 YES，则是对应的消息，您可以在 UI 上执行删除操作，具体可以参考 TMessageController.h 里面的 onRevokeMessage 回调的实现。
  *
  *  @param msg   被撤回的消息
  *  @param succ  成功时回调
  *  @param fail  失败时回调
  *
- *  @return 0 本次操作成功
+ *  @return 0：本次操作成功；1：本次操作失败
  */
 - (int)revokeMessage:(TIMMessage*)msg succ:(TIMSucc)succ fail:(TIMFail)fail;
 
 /**
- *  同步本会话的消息撤回通知（仅GROUP会话有效）
+ *  同步群的消息撤回通知
  *
- *  @param succ  成功时回调，同步的通知会通过TIMMessageRevokeListener抛出
+ *  针对群组，断线重连后，如果用户处于群组聊天界面，需要业务端主动同步该群组会话的消息撤回通知。其他场景不需要主动同步消息撤回通知。
+ *
+ *  @param succ  成功时回调，同步的通知会通过 TIMMessageRevokeListener 抛出
  *  @param fail  失败时回调
  *
  *  @return 0 本次操作成功
@@ -95,6 +112,8 @@
 
 /**
  *  删除本地会话消息
+ *
+ *  可以在保留会话同时删除本地的会话消息。
  *
  *  @param succ  成功时回调
  *  @param fail  失败时回调
@@ -106,7 +125,11 @@
 /**
  *  设置已读消息
  *
- *  @param readed 会话内最近一条已读的消息，nil表示上报最新消息
+ *  1. 如果在 TIMUserConfig 关闭了自动已读上报（设置 disableAutoReport 为 YES），这里需要显示调用 setReadMessage ，消息的已读状态才会改变。
+ *  2. 如果在 TIMUserConfig 开启了消息回执（设置 enableReadReceipt 为 YES），收消息的用户需要显示调用 setReadMessage ，发消息的用户才能通过 TIMMessageReceiptListener 监听到消息的已读回执。
+ *  3. 如果您需要设置单条消息的已读状态，请使用 TIMMessage+MsgExt.h 中的 setCustomInt 自定义字段接口设置，通过 customInt 获取，需要注意的是，这个接口设置的字段只在本地保存，如果切换了终端，或则 APP 被卸载，这个值都会失效。
+ *
+ *  @param readed 会话内最近一条已读的消息，nil 表示上报最新消息
  *
  *  @param succ  成功时回调
  *  @param fail  失败时回调
@@ -116,7 +139,10 @@
 - (int)setReadMessage:(TIMMessage*)readed succ:(TIMSucc)succ fail:(TIMFail)fail;
 
 /**
- *  获取该会话的未读计数
+ *  获取会话的未读计数
+ *
+ *  1. 单终端，未卸载 APP 的情况下，您可以通过当前接口获取准确的未读计数。
+ *  2. 如果需要在多终端或则程序 APP 卸载重装后未读计数依然准确，请设置 TIMUserConfig 的 disableAutoReport 为 YES，然后显示调用 setReadMessage。详情请参考 [未读消息计数]（https://cloud.tencent.com/document/product/269/9151）
  *
  *  @return 返回未读计数
  */
@@ -134,29 +160,33 @@
  *
  *  @param msgs 消息（TIMMessage*）列表
  *
- *  @return 0 成功
+ *  @return 0：成功；1：失败
  */
 - (int)importMessages:(NSArray*)msgs;
 
 /**
  *  设置会话草稿
  *
- *  @param draft 草稿内容
+ *  UI 展示最近联系人列表时，时常会展示用户的草稿内容，ImSDK 提供了设置和获取草稿的接口，用户可以通过此接口设置会话的草稿信息。草稿信息会存本地数据库，重新登录后依然可以获取。
  *
- *  @return 0 成功
+ *  @param draft 草稿内容，详情请参考 TIMMessage+MsgExt.h 里面的 TIMMessageDraft 定义
+ *
+ *  @return 0：成功；1：失败
  */
 - (int)setDraft:(TIMMessageDraft*)draft;
 
 /**
  *  获取会话草稿
  *
- *  @return 草稿内容，没有草稿返回nil
+ *  @return 草稿内容，没有草稿返回 nil
  */
 - (TIMMessageDraft*)getDraft;
 
 /**
- * 禁用本会话的存储，只对当前初始化有效，重启后需要重新设置
- * 需要 initSdk 之后调用
+ *  禁用本会话的存储(暂未实现)
+ *
+ *  1. 需要 initSdk 之后调用
+ *  2. 只对当前初始化有效，重启后需要重新设置
  */
 - (void)disableStorage;
 
--- Mac/ImSDKForMac.framework/Versions/A/Headers/TIMFriendshipDefine.h
+++ Mac/ImSDKForMac.framework/Versions/A/Headers/TIMFriendshipDefine.h
@@ -12,8 +12,10 @@
 #import "ImSDK.h"
 
 @class TIMFriendMetaInfo;
-@class TIMFriendPendencyMeta;
+@class TIMFriendPendencyResponse;
+@class TIMFriendPendencyItem;
 @class TIMFriendFutureMeta;
+@class TIMFriendGroup;
 
 #pragma mark - 枚举类型
 
@@ -26,6 +28,25 @@ typedef NS_ENUM(NSInteger, TIMFriendStatus) {
      */
     TIM_FRIEND_STATUS_SUCC                              = 0,
     
+    /**
+     *  请求参数错误，请根据错误描述检查请求是否正确
+     */
+    TIM_FRIEND_PARAM_INVALID                                = 30001,
+    
+    /**
+     *  加好友、响应好友时有效：自己的好友数已达系统上限
+     */
+    TIM_ADD_FRIEND_STATUS_SELF_FRIEND_FULL                  = 30010,
+    
+    /**
+     * 更新好友分组时有效：分组已达系统上限
+     */
+    TIM_UPDATE_FRIEND_GROUP_STATUS_MAX_GROUPS_EXCEED        = 30011,
+    
+    /**
+     *  加好友、响应好友时有效：对方的好友数已达系统上限
+     */
+    TIM_ADD_FRIEND_STATUS_THEIR_FRIEND_FULL                 = 30014,
     
     /**
      *  加好友时有效：被加好友在自己的黑名单中
@@ -36,16 +57,6 @@ typedef NS_ENUM(NSInteger, TIMFriendStatus) {
      *  加好友时有效：被加好友设置为禁止加好友
      */
     TIM_ADD_FRIEND_STATUS_FRIEND_SIDE_FORBID_ADD            = 30516,
-    
-    /**
-     *  加好友时有效：好友数量已满
-     */
-    TIM_ADD_FRIEND_STATUS_SELF_FRIEND_FULL                  = 30519,
-    
-    /**
-     *  加好友时有效：已经是好友
-     */
-    TIM_ADD_FRIEND_STATUS_ALREADY_FRIEND                    = 30520,
     
     /**
      *  加好友时有效：已被被添加好友设置为黑名单
@@ -67,27 +78,10 @@ typedef NS_ENUM(NSInteger, TIMFriendStatus) {
      */
     TIM_DEL_FRIEND_STATUS_NO_FRIEND                         = 31704,
     
-    
     /**
      *  响应好友申请时有效：对方没有申请过好友
      */
     TIM_RESPONSE_FRIEND_STATUS_NO_REQ                       = 30614,
-    
-    /**
-     *  响应好友申请时有效：自己的好友满
-     */
-    TIM_RESPONSE_FRIEND_STATUS_SELF_FRIEND_FULL             = 30615,
-    
-    /**
-     *  响应好友申请时有效：好友已经存在
-     */
-    TIM_RESPONSE_FRIEND_STATUS_FRIEND_EXIST                 = 30617,
-    
-    /**
-     *  响应好友申请时有效：对方好友满
-     */
-    TIM_RESPONSE_FRIEND_STATUS_OTHER_SIDE_FRIEND_FULL       = 30630,
-    
     
     /**
      *  添加黑名单有效：已经在黑名单了
@@ -129,10 +123,7 @@ typedef NS_ENUM(NSInteger, TIMFriendStatus) {
      */
     TIM_UPDATE_FRIEND_GROUP_STATUS_DEL_NOT_IN_GROUP         = 32520,
     
-    /**
-     * 更新好友分组时有效：该好友加入的好友分组个数超过了限制，每个好友最多只能加入32个好友分组
-     */
-    TIM_UPDATE_FRIEND_GROUP_STATUS_MAX_GROUPS_EXCEED        = 32521,
+    
 };
 
 typedef NS_ENUM(NSInteger, TIMDelFriendType) {
@@ -147,21 +138,21 @@ typedef NS_ENUM(NSInteger, TIMDelFriendType) {
     TIM_FRIEND_DEL_BOTH                 = 2,
 };
 
-typedef NS_ENUM(NSInteger, TIMPendencyGetType) {
+typedef NS_ENUM(NSInteger, TIMPendencyType) {
     /**
      *  别人发给我的
      */
-    TIM_PENDENCY_GET_COME_IN                    = 1,
+    TIM_PENDENCY_COME_IN                    = 1,
     
     /**
      *  我发给别人的
      */
-    TIM_PENDENCY_GET_SEND_OUT                   = 2,
+    TIM_PENDENCY_SEND_OUT                   = 2,
     
     /**
-     * 别人发给我的 和 我发给别人的
+     * 别人发给我的 和 我发给别人的。仅拉取时有效
      */
-    TIM_PENDENCY_GET_BOTH                       = 3,
+    TIM_PENDENCY_BOTH                       = 3,
 };
 
 /**
@@ -270,18 +261,9 @@ typedef void (^TIMGetFriendListByPageSucc)(TIMFriendMetaInfo * meta, NSArray * f
 /**
  * 获取未决请求列表成功
  *
- *  @param meta 未决请求元信息
- *  @param pendencies  未决请求列表（TIMFriendPendencyItem*）数组
+ *  @param pendencyResponse 未决请求元信息
  */
-typedef void (^TIMGetFriendPendencyListSucc)(TIMFriendPendencyMeta * meta, NSArray * pendencies);
-
-/**
- * 获取推荐好友和未决列表成功
- *
- *  @param meta      下次拉取时填入信息
- *  @param items     列表（TIMFriendFutureItem*）数组
- */
-typedef void (^TIMGetFriendFutureListSucc)(TIMFriendFutureMeta * meta, NSArray * items);
+typedef void (^TIMGetFriendPendencyListSucc)(TIMFriendPendencyResponse *pendencyResponse);
 
 /**
  *  群搜索回调
@@ -294,9 +276,9 @@ typedef void (^TIMUserSearchSucc)(uint64_t totalNum, NSArray * users);
 /**
  *  好友分组列表
  *
- *  @param arr 好友分组（TIMFriendGroup*)列表
+ *  @param groups 好友分组（TIMFriendGroup*)列表
  */
-typedef void (^TIMFriendGroupSucc)(NSArray * arr);
+typedef void (^TIMFriendGroupArraySucc)(NSArray<TIMFriendGroup *> * groups);
 
 /**
  *  好友关系检查回调
@@ -310,32 +292,33 @@ typedef void (^TIMFriendCheckSucc)(NSArray* results);
 /**
  *  加好友请求
  */
-@interface TIMAddFriendRequest : TIMCodingModel
+@interface TIMFriendRequest : TIMCodingModel
 
 /**
- *  用户identifier
+ *  用户identifier（必填）
  */
-@property(nonatomic,strong) NSString* identifier;
+@property (nonatomic,strong) NSString* identifier;
 
 /**
- *  用户备注（备注最大96字节）
+ *  备注（备注最大96字节）
  */
-@property(nonatomic,strong) NSString* remark;
+@property (nonatomic,strong) NSString* remark;
 
 /**
  *  请求说明（最大120字节）
  */
-@property(nonatomic,strong) NSString* addWording;
+@property (nonatomic,strong) NSString* addWording;
 
 /**
  *  添加来源
+ *  来源需要添加“AddSource_Type_”前缀
  */
-@property(nonatomic,strong) NSString* addSource;
+@property (nonatomic,strong) NSString* addSource;
 
 /**
- *  预分组名
+ *  分组
  */
-@property(nonatomic,strong) NSString* friendGroup;
+@property (nonatomic,strong) NSString* group;
 
 @end
 
@@ -369,14 +352,37 @@ typedef void (^TIMFriendCheckSucc)(NSArray* results);
 /**
  * 未决请求类型
  */
-@property(nonatomic,assign) TIMPendencyGetType type;
+@property(nonatomic,assign) TIMPendencyType type;
 
 @end
 
 /**
- * 未决请求元信息
+ * 未决推送
  */
-@interface TIMFriendPendencyMeta : TIMCodingModel
+@interface TIMFriendPendencyInfo : TIMCodingModel
+/**
+ * 用户标识
+ */
+@property(nonatomic,strong) NSString* identifier;
+/**
+ * 来源
+ */
+@property(nonatomic,strong) NSString* addSource;
+/**
+ * 加好友附言
+ */
+@property(nonatomic,strong) NSString* addWording;
+
+/**
+ * 加好友昵称
+ */
+@property(nonatomic,strong) NSString* nickname;
+@end
+
+/**
+ * 未决请求信息
+ */
+@interface TIMFriendPendencyRequest : TIMCodingModel
 
 /**
  * 序列号，未决列表序列号
@@ -392,84 +398,45 @@ typedef void (^TIMFriendCheckSucc)(NSArray* results);
 @property(nonatomic,assign) uint64_t timestamp;
 
 /**
- * 每页的数量，请求时有效（建议值，server可根据需要返回或多或少，不能作为完成与否的标志）
+ * 每页的数量，即本次请求最多返回都个数据
  */
 @property(nonatomic,assign) uint64_t numPerPage;
 
 /**
- * 未决请求未读数量（仅在server返回时有效）
+ * 未决请求拉取类型
  */
-@property(nonatomic,assign) uint64_t unReadCnt;
+@property(nonatomic,assign) TIMPendencyType type;
 
 @end
 
-
 /**
- * 推荐好友元信息
+ * 未决返回信息
  */
-@interface TIMFriendFutureMeta : TIMCodingModel
+@interface TIMFriendPendencyResponse : TIMCodingModel
 
 /**
- * 翻页类型
+ * 本次请求的未决列表序列号
  */
-@property(nonatomic,assign) TIMPageDirectionType directionType;
+@property(nonatomic,assign) uint64_t seq;
 
 /**
- * 获取数量
- */
-@property(nonatomic,assign) uint64_t reqNum;
-
-/**
- * 时间戳：只做分页之用，第一次请求填0，分页时下次请求传入返回的时间戳，直到返回的时间戳为0，表示数据已经拉完，此时更新pendencySeq和recommendSeq（分页过程中不能更新pendencySeq和recommendSeq）
+ * 本次请求的翻页时间戳
  */
 @property(nonatomic,assign) uint64_t timestamp;
 
 /**
- * 未决序列号
+ * 未决请求未读数量
  */
-@property(nonatomic,assign) uint64_t pendencySeq;
+@property(nonatomic,assign) uint64_t unreadCnt;
 
 /**
- * 推荐序列号
+ * 未决数据
  */
-@property(nonatomic,assign) uint64_t recommendSeq;
-
-/**
- * 已决序列号
- */
-@property(nonatomic,assign) uint64_t decideSeq;
-
-/**
- * 未决未读数量
- */
-@property(nonatomic,assign) uint64_t pendencyUnReadCnt;
-
-/**
- * 推荐未读数量
- */
-@property(nonatomic,assign) uint64_t recommendUnReadCnt;
-
-/**
- * 已决未读数量
- */
-@property(nonatomic,assign) uint64_t decideUnReadCnt;
-
-/**
- * 未决最新时间戳
- */
-@property(nonatomic,assign) uint64_t currentPendencyTimestamp;
-
-/**
- * 推荐最新时间戳
- */
-@property(nonatomic,assign) uint64_t currentRecommendTimestamp;
-
-/**
- * 已决最新时间戳
- */
-@property(nonatomic,assign) uint64_t currentDecideTimestamp;
+@property NSArray<TIMFriendPendencyItem *> * pendencies;
 
 @end
+
+
 
 /**
  * 好友元信息
@@ -553,14 +520,23 @@ typedef void (^TIMFriendCheckSucc)(NSArray* results);
 /**
  *  用户Id
  */
-@property(nonatomic,strong) NSString* identifier;
+@property NSString* identifier;
+
 /**
- *  返回状态
+ * 返回码
  */
-@property(nonatomic,assign) TIMFriendStatus status;
+@property NSInteger result_code;
+
+/**
+ * 返回信息
+ */
+@property NSString *result_info;
 
 @end
 
+/**
+ * 响应好友请求
+ */
 @interface TIMFriendResponse : NSObject
 
 /**
@@ -574,53 +550,12 @@ typedef void (^TIMFriendCheckSucc)(NSArray* results);
 @property(nonatomic,strong) NSString* identifier;
 
 /**
- *  （可选）如果要加对方为好友，表示备注，其他type无效，备注最大96字节
+ *  备注好友（可选，如果要加对方为好友）。备注最大96字节
  */
 @property(nonatomic,strong) NSString* remark;
 
 @end
 
-/**
- * 推荐好友
- */
-@interface TIMFriendFutureItem : TIMCodingModel
-
-/**
- * 推荐好友类型
- */
-@property(nonatomic,assign) TIMFutureFriendType type;
-
-/**
- * 好友标识
- */
-@property(nonatomic,strong) NSString* identifier;
-
-/**
- * 好友资料
- */
-@property(nonatomic,strong) TIMUserProfile* profile;
-
-/**
- * 添加时间
- */
-@property(nonatomic,assign) uint64_t addTime;
-
-/**
- * 来源（仅未决好友有效）
- */
-@property(nonatomic,strong) NSString* addSource;
-
-/**
- * 加好友附言（仅未决好友有效）
- */
-@property(nonatomic,strong) NSString* addWording;
-
-/**
- * 推荐理由（server端写入，仅推荐好友有效）
- */
-@property(nonatomic,strong) NSDictionary* recommendTags;
-
-@end
 
 /**
  *  好友分组信息扩展
@@ -632,7 +567,7 @@ typedef void (^TIMFriendCheckSucc)(NSArray* results);
 @property(nonatomic,strong) NSArray* profiles;
 @end
 
-
+// 用户资料KEY
 
 /**
  * 昵称
@@ -656,17 +591,17 @@ extern NSString *const TIMProfileTypeKey_AllowType;
 extern NSString *const TIMProfileTypeKey_Gender;
 /**
  * 生日
- * 值类型: NSNumber 
+ * 值类型: NSNumber
  */
 extern NSString *const TIMProfileTypeKey_Birthday;
 /**
  * 位置
- * 值类型: NSString 
+ * 值类型: NSString
  */
 extern NSString *const TIMProfileTypeKey_Location;
 /**
  * 语言
- * 值类型: NSNumber 
+ * 值类型: NSNumber
  */
 extern NSString *const TIMProfileTypeKey_Language;
 /**
@@ -676,7 +611,7 @@ extern NSString *const TIMProfileTypeKey_Language;
 extern NSString *const TIMProfileTypeKey_Level;
 /**
  * 角色
- * 值类型: NSNumber 
+ * 值类型: NSNumber
  */
 extern NSString *const TIMProfileTypeKey_Role;
 /**
@@ -686,8 +621,27 @@ extern NSString *const TIMProfileTypeKey_Role;
 extern NSString *const TIMProfileTypeKey_SelfSignature;
 /**
  * 自定义字段前缀
- * 值类型: [NSData|NSNumber]
+ * 值类型: [NSString,NSData|NSNumber]
+ * @note 当设置自定义字的值NSString对象时，后台会将其转为UTF8保存在数据库中。由于部分用户迁移资料时可能不是UTF8类型，所以在获取资料时，统一返回NSData类型。
  */
 extern NSString *const TIMProfileTypeKey_Custom_Prefix;
+
+// 好友资料KEY
+
+/**
+ * 备注
+ * 值类型: NSString
+ */
+extern NSString *const TIMFriendTypeKey_Remark;
+/**
+ * 分组
+ * 值类型: [NSArray]
+ */
+extern NSString *const TIMFriendTypeKey_Group;
+/**
+ * 自定义字段前缀
+ * 值类型: [NSString,NSData|NSNumber]
+ */
+extern NSString *const TIMFriendTypeKey_Custom_Prefix;
 #endif /* TIMFriendshipDefine_h */
 
