4ba28863e472d3b6457c5bf9abf4ff5f736ec6b5
DianQK <dianqk@icloud.com>
2019-07-24 18:57:52 +0800

    build: 修复 TXIMSDK_TUIKit_iOS.podspec 头文件描述不合适导致 Swift 直接 import 失败问题

---
+++ iOS/TUIKitSwiftDemo/TUIKitSwiftDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TUIKitSwiftDemo
+//
+//  Created by dianqk on 2019/7/24.
+//  Copyright © 2019 Tencent. All rights reserved.
+//
+
+import UIKit
+import TXIMSDK_TUIKit_iOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
