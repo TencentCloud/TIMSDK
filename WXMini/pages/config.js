96bc02e82d14ff6a76ef31efd31a6d0027060b7f
irisZhangYuwei <irisywzhang@tencent.com>
2019-08-05 21:30:31 +0800

    fix(wixin): 修正小程序usersig计算方法

--- WXMini/pages/config.js
+++ WXMini/pages/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
 
     // 2019/8/5 
     // 更新登录方式
-    // 在 ../debug/GenerateTestUserSig.js 里配置好自己的 sdkappid 和 privateKey 直接就可登录，无需手动生成 identifier 和 usersig
+    // 可以直接在utils下的 GenerateTestUserSig.js 里配置好自己的 sdkappid 和 privateKey 直接就可登录，无需手动生成 identifier 和 usersig
 
     app: {
         sdkappid : '' // 填入创建腾讯云通讯应用获取到的 sdkappid
