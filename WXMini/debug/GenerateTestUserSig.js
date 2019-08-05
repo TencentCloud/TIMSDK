e314003374bb5f61804a4e2c4dd21927369a526c
zhangyuiris <913007768zyw@gmail.com>
2019-08-05 21:38:51 +0800

    fix(wixin):修改为空串

--- WXMini/debug/GenerateTestUserSig.js
+++ WXMini/debug/GenerateTestUserSig.js
@@ -46,7 +46,7 @@ function genTestUserSig(userID) {
    * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
    * 文档：https://cloud.tencent.com/document/product/647/17275#Server
    */
-  var PRIVATEKEY = "XXXXXXXXXXX";
+  var PRIVATEKEY = "";
 
   var generator = new LibGenerateTestUserSig(SDKAPPID, PRIVATEKEY, EXPIRETIME);
   var userSig = generator.genTestUserSig(userID);
