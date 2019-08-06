b22e9ba77f99a5c996a8b1710580ee88daa89964
lynxzhang <lynxzhang1988@gmail.com>
2019-08-06 19:33:27 +0800

    Mac Demo 更新

--- Mac/TUIKitDemo/TUIKitDemo/AppDelegate.h
+++ Mac/TUIKitDemo/TUIKitDemo/AppDelegate.h
@@ -10,5 +10,6 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
+
 @end
 
--- Mac/TUIKitDemo/TUIKitDemo/Debug/GenerateTestUserSig.h
+++ Mac/TUIKitDemo/TUIKitDemo/Debug/GenerateTestUserSig.h
@@ -26,7 +26,7 @@ NS_ASSUME_NONNULL_BEGIN
  * 进入腾讯云云通信[控制台](https://console.cloud.tencent.com/avc) 创建应用，即可看到 SDKAppId，
  * 它是腾讯云用于区分客户的唯一标识。
  */
-static const int SDKAPPID = 1400187352;
+static const int SDKAPPID = 0;
 
 /**
  *  签名过期时间，建议不要设置的过短
@@ -46,7 +46,7 @@ static const int EXPIRETIME = 604800;
  * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
  * 文档：https://cloud.tencent.com/document/product/269/32688#Server
  */
-static NSString * const SECRETKEY = @"61cbf613d0cea4b302958e39c7b74acaaed0956fe8c494eda1c45912c324ecab";
+static NSString * const SECRETKEY = @"";
 
 
 @interface GenerateTestUserSig : NSObject
