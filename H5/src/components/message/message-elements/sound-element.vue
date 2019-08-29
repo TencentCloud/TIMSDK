e922db46ab5cc1b28dc100d7684e90c9348a103b
rychouwei <rychouwei@tencent.com>
2019-08-29 15:27:34 +0800

    dev: 优化语音消息播放逻辑

--- H5/src/components/message/message-elements/sound-element.vue
+++ H5/src/components/message/message-elements/sound-element.vue
@@ -32,21 +32,48 @@ export default {
   },
   methods: {
     play() {
+      // 目前移动端的语音消息采用 aac 格式，以前用 amr 格式。默认先用 audio 标签播放，若无法播放则尝试 amr 格式播放。
+      const audio = document.createElement('audio')
+      audio.addEventListener('error', this.tryPlayAMR) // 播放出错，则尝试使用 AMR 播放
+      audio.src = this.url
+      const promise = audio.play()
+      if (promise) {
+        promise.catch(() => {})
+      }
+    },
+    tryPlayAMR() {
       try {
-        if (!this.amr) {
-          const BenzAMRRecorder = require('benz-amr-recorder')
-          this.amr = new BenzAMRRecorder()
-        }
-        if (this.amr.isInit()) {
-          this.amr.play()
+        const isIE = /MSIE|Trident|Edge/.test(window.navigator.userAgent)
+        // amr 播放组件库在 IE 不支持
+        if (isIE) {
+          this.$message.warning('您的浏览器不支持该格式的语音消息播放，请尝试更换浏览器，建议使用：谷歌浏览器')
           return
         }
-        this.amr.initWithUrl(this.url).then(() => {
-          this.amr.play()
-        })
+        // 动态插入 amr 播放组件库
+        if (!window.BenzAMRRecorder) {
+          const script = document.createElement('script')
+          script.addEventListener('load', this.playAMR)
+          script.src = './BenzAMRRecorder.js'
+          const firstScript = document.getElementsByTagName('script')[0]
+          firstScript.parentNode.insertBefore(script, firstScript)
+          return
+        }
+        this.playAMR()
       } catch (error) {
-        this.$message.warn('您的浏览器不支持amr格式的语音播放')
+        this.$message.warning('您的浏览器不支持该格式的语音消息播放，请尝试更换浏览器，建议使用：谷歌浏览器')
       }
+    },
+    playAMR() {
+      if (!this.amr && window.BenzAMRRecorder) {
+        this.amr = new window.BenzAMRRecorder()
+      }
+      if (this.amr.isInit()) {
+        this.amr.play()
+        return
+      }
+      this.amr.initWithUrl(this.url).then(() => {
+        this.amr.play()
+      })
     }
   }
 }
