0573b3655bffa6174254290d1f72cea98ef68080
rychouwei <rychouwei@tencent.com>
2019-09-04 10:35:09 +0800

    dev: 移除一处不用的配置并更新 dist

--- H5/src/utils/common.js
+++ H5/src/utils/common.js
@@ -25,12 +25,3 @@ export function translateGroupSystemNotice(message) {
       return '自定义群系统通知'
   }
 }
-
-export const errorMap = {
-  500: '服务器错误',
-  602: '用户名或密码不合法',
-  610: '用户名格式错误',
-  612: '用户已存在',
-  620: '用户不存在',
-  621: '密码错误'
-}
