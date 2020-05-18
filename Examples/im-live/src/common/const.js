3b5fdc31609bc059fa5d5025de037b9b7a82370e
xingchenhe <xingchenhe@xingchenhe-mb1.tencent.com>
2020-05-18 10:21:57 +0800

    直播带货demo去掉没用的测试数据

--- Examples/im-live/src/common/const.js
+++ Examples/im-live/src/common/const.js
@@ -1,4 +1,4 @@
 export default {
-    HOST:"https://service-qr8jjnpm-1256635546.gz.apigw.tencentcs.com",
-    IMSDKAPPID:1400357589
+    HOST:"https://test.com",//这里是业务域名
+    IMSDKAPPID:12345678 //这里是腾讯云IMsdkappid
 }
