5d385c4c1314b2d30d883f58e1246cf93bc31d96
junker <zhaofei.one@qq.com>
2020-10-16 14:51:14 +0800

    修复tuikit处于聊天页面且正在接收对方语音消息时，语音消息图标显示错乱问题

--- Android/tuikit/src/main/java/com/tencent/qcloud/tim/uikit/modules/chat/layout/message/holder/MessageAudioHolder.java
+++ Android/tuikit/src/main/java/com/tencent/qcloud/tim/uikit/modules/chat/layout/message/holder/MessageAudioHolder.java
@@ -47,9 +47,9 @@ public class MessageAudioHolder extends MessageContentHolder {
 
     @Override
     public void initVariableViews() {
-        audioTimeText = rootView.findViewById(R.id.audio_time_tv);
-        audioPlayImage = rootView.findViewById(R.id.audio_play_iv);
-        audioContentView = rootView.findViewById(R.id.audio_content_ll);
+            audioTimeText = rootView.findViewById(R.id.audio_time_tv);
+            audioPlayImage = rootView.findViewById(R.id.audio_play_iv);
+            audioContentView = rootView.findViewById(R.id.audio_content_ll);
     }
 
     @Override
@@ -57,6 +57,7 @@ public class MessageAudioHolder extends MessageContentHolder {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.addRule(RelativeLayout.CENTER_VERTICAL);
+
         if (msg.isSelf()) {
             params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
             params.rightMargin = 24;
@@ -71,7 +72,8 @@ public class MessageAudioHolder extends MessageContentHolder {
             // TODO 图标不对
             audioPlayImage.setImageResource(R.drawable.voice_msg_playing_3);
             audioContentView.removeView(audioPlayImage);
-            audioContentView.addView(audioPlayImage, 0);
+            audioPlayImage.setRotation(0f);
+            audioContentView.addView(audioPlayImage,0);
             if (msg.getCustomInt() == UNREAD) {
                 LinearLayout.LayoutParams unreadParams = (LinearLayout.LayoutParams) isReadText.getLayoutParams();
                 unreadParams.gravity = Gravity.CENTER_VERTICAL;
