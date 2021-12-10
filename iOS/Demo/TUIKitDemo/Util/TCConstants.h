c50b819d2fcfd2b3b5fd273c0c4b2d96b781d16f
lynxzhang <lynxzhang1988@gmail.com>
2021-12-10 16:54:13 +0800

    IM SDK 增强版发布5.8.1696版本

--- iOS/Demo/TUIKitDemo/Util/TCConstants.h
+++ iOS/Demo/TUIKitDemo/Util/TCConstants.h
@@ -10,25 +10,25 @@
 #define TCConstants_h
 
 // Http配置
-#define kHttpServerAddr @""
+#define kHttpServerAddr @"https://im-demo.qcloud.com"
 
 // 图形验证码地址
-#define kHttpSmsImageAddr @""
+#define kHttpSmsImageAddr @"https://service-c2zjvuxa-1252463788.gz.apigw.tencentcs.com/release/smsImg"
 
 // 登录地址 - 公有云
-#define kHttpSmsLoginAddr_public @""
+#define kHttpSmsLoginAddr_public @"https://service-c2zjvuxa-1252463788.gz.apigw.tencentcs.com/release/demoSms"
 
 // 登录地址 - 新加坡
-#define kHttpSmsLoginAddr_singapore @""
+#define kHttpSmsLoginAddr_singapore @"https://service-c2zjvuxa-1252463788.gz.apigw.tencentcs.com/release/demoSmsInter"
 
 //Elk 数据上报地址
-#define DEFAULT_ELK_HOST @""
+#define DEFAULT_ELK_HOST @"https://ilivelog.qcloud.com"
 
 //Licence url
-#define LicenceURL @""
+#define LicenceURL @"http://license.vod2.myqcloud.com/license/v1/3e0ffdeec6f69f2ebb0033705d4b33e5/TXLiveSDK.licence"
 
 //Licence key
-#define LicenceKey @""
+#define LicenceKey @"439c61a21e71e1af31024c2700f95b52"
 
 //**********************************************************************
 
