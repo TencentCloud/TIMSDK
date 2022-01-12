194caad67c3d07059de77546bb85b364c73dc11e
anonymous <anonymous>
2022-01-12 19:48:17 +0800

    更新flutter仓库

--- Flutter/Demo/TencentIMFlutterDemo/android/app/src/main/java/com/qq/qcloud/TencentImSDKPlugin_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.qq.qcloud.tencent_im_sdk_plugin_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- Flutter/Demo/TencentIMFlutterDemo/lib/utils/config.dart
+++
@@ -1,6 +0,0 @@
-class Config {
-  static const int sdkappid = int.fromEnvironment('SDK_APPID', defaultValue: 0);
-  static const String key = String.fromEnvironment('KEY', defaultValue: "");
-  static const bool productEnv =
-      bool.fromEnvironment('ISPRODUCT_ENV', defaultValue: false);
-}
