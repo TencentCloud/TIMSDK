c5d429cbe60446eeb55aa7bb2770b53787e4ede6
anonymous <anonymous>
2022-04-08 17:42:06 +0800

    update flutter uikit、electron、unity

--- Flutter/Demo/im-flutter-uikit/lib/main.dart
+++ Flutter/Demo/im-flutter-uikit/lib/main.dart
@@ -1,14 +1,17 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-// import 'package:flutter_autosize_screen/auto_size_util.dart';
-// import 'package:flutter_autosize_screen/binding.dart';
+import 'package:flutter_baidu_mapapi_base/flutter_baidu_mapapi_base.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tim_ui_kit/ui/utils/color.dart';
 import 'package:tim_ui_kit_calling_plugin/tim_ui_kit_calling_plugin.dart';
 import 'package:timuikit/custom_animation.dart';
 import 'package:timuikit/i18n/strings.g.dart';
+import 'package:timuikit/src/config.dart';
 import 'package:timuikit/src/pages/app.dart';
+import 'package:timuikit/src/provider/custom_sticker_package.dart';
+import 'package:timuikit/src/provider/login_user_Info.dart';
 import 'package:timuikit/src/provider/theme.dart';
 
 import 'package:provider/provider.dart';
@@ -25,6 +28,15 @@ void main() {
   // fast i18n use device locale
   WidgetsFlutterBinding.ensureInitialized();
   LocaleSettings.useDeviceLocale();
+
+  if (Platform.isIOS) {
+    BMFMapSDK.setApiKeyAndCoordType(
+        IMDemoConfig.baiduMapIOSAppKey, BMF_COORD_TYPE.BD09LL);
+  } else if (Platform.isAndroid) {
+    BMFMapSDK.setCoordType(BMF_COORD_TYPE.BD09LL);
+  }
+  BMFMapSDK.setAgreePrivacy(true);
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(
@@ -32,7 +44,9 @@ void main() {
       TranslationProvider(
         child: MultiProvider(
           providers: [
-            ChangeNotifierProvider(create: (_) => DefaultThemeData())
+            ChangeNotifierProvider(create: (_) => LoginUserInfo()),
+            ChangeNotifierProvider(create: (_) => DefaultThemeData()),
+            ChangeNotifierProvider(create: (_) => CustomStickerPackageData()),
           ],
           child: const TUIKitDemoApp(),
         ),
