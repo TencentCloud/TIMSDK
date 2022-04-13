68d7cd8ff16c2baa9c53068a8f4bd551c8ed9472
anonymous <anonymous>
2022-04-13 19:38:02 +0800

    update flutter uikit demo to 0.0.6-bug-fix-0.1

--- Flutter/Demo/im-flutter-uikit/package_src/tim_ui_kit/lib/ui/constants/time.dart
+++
@@ -1,7 +0,0 @@
-class TimeConst {
-  static const DAY_SEC = 86400;
-  static const HOUR_SEC = 3600;
-  static const MIN_SEC = 60;
-
-  static const SEC_SERIES = [HOUR_SEC, MIN_SEC];
-}
--- Flutter/Demo/im-flutter-uikit/package_src/tim_ui_kit/lib/ui/views/TIMUIKitChat/TIMUIKitMessageItem/main.dart
+++
@@ -1,8 +0,0 @@
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_file_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_group_tips_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_image_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_sound_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_text_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_video_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_merger_message_elem.dart';
-export 'package:tim_ui_kit/ui/views/TIMUIKitChat/TIMUIKitMessageItem/tim_uikit_chat_reply_elem.dart';
