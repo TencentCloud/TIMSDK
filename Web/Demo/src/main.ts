30919d5268dd716d62036d0e6846fb41613f7e1e
angular2 <tyrosning@gmail.com>
2022-05-26 10:56:37 +0800

    feat: add tim-upload-plugin (#1169)
    feat: tim-upload-plugin
    
    Co-authored-by: tyrosning <tyrosning@tencent.com>

--- Web/Demo/src/main.ts
+++ Web/Demo/src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-// import TIM from "tim-js-sdk";
 
 import { SDKAPPID }  from '../debug';
 
@@ -12,17 +11,10 @@ import 'element-plus/dist/index.css';
 
 import locales from './locales';
 
-// const options = {
-//   SDKAppID: 1400187352 // 接入时需要将0替换为您的云通信应用的 SDKAppID，类型为 Number
-// };
-// const tim = TIM.create(options);
-
-// import { TUIChat, TUIConversation } from './TUIKit/TUIComponents';
 import { TUICore, TUIComponents } from './TUIKit';
 
 const config = {
   SDKAppID: SDKAPPID,
-  // tim,
 };
 
 const TUIKit = TUICore.init(config);
@@ -31,7 +23,7 @@ TUIKit.config.i18n.provideMessage(locales);
 
 TUIComponents.TUIChat.removePluginComponents(['Custom', 'Location']);
 
-// 组件挂载到 TUIKit
+// component plugin in TUIKit
 TUIKit.use(TUIComponents);
 
 const app = createApp(App);
