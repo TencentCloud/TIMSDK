cfb1bbcc40a8be77aa97c319bfd722bf7faeca8c
何星辰 <hexingcheng@users.noreply.github.com>
2022-07-20 15:33:01 +0800

    Delete Flutter/Demo/im_api_example directory
    delete im_flutter_api_example

--- Flutter/Demo/im_api_example/android/app/src/main/java/com/example/im_api_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.tencent.qcloud.tim.uikit;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- Flutter/Demo/im_api_example/android/app/src/main/kotlin/tencent/imApiExample/im_api_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package tencent.imApiExample.im_api_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
