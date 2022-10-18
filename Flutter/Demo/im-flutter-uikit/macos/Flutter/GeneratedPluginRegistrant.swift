24508fe2b38ac3f9f69da5b6493e258eb5eac627
anonymous <anonymous>
2022-10-18 15:42:36 +0800

    update flutter uikit demo to 0.1.7

--- Flutter/Demo/im-flutter-uikit/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/Demo/im-flutter-uikit/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus_macos
 import device_info_plus_macos
 import firebase_core
 import firebase_messaging
@@ -21,7 +20,6 @@ import url_launcher_macos
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
