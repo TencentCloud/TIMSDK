01c4b26a91169e6edf2ddd280348629b76c406cc
SummerHaze <wobushisummer@gmail.com>
2025-11-10 16:11:40 +0800

    Remove unused AssetsLibrary import
    Removed unused AssetsLibrary import.

--- iOS/TUIKit/TUIChat/UI_Minimalist/Chat/TUIBaseChatViewController_Minimalist.m
+++ iOS/TUIKit/TUIChat/UI_Minimalist/Chat/TUIBaseChatViewController_Minimalist.m
@@ -8,7 +8,6 @@
 
 #import "TUIBaseChatViewController_Minimalist.h"
 #import <AVFoundation/AVFoundation.h>
-#import <AssetsLibrary/AssetsLibrary.h>
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <Photos/Photos.h>
 #import <TIMCommon/NSString+TUIEmoji.h>
